6ecb836d6ca591af886419a3a84f28f63c820221
Andrea <andreagrimmiger1@hotmail.com>
2020-01-22 16:57:41 +0100

    Update game_Easy.js

--- game_Easy.js
+++ game_Easy.js
@@ -1,3 +1,14 @@
+/**********************************************************     GLOBALE VARIABLEN   **********************************************/
+var background;           
+var catcher;                                                    // Komponenten des Spiels
+var timer, t, sec, intervalTime;                                // Komponenten für den Timer
+var counter, count;                                             // Komponenten für die Punkte
+var btn_back;                                                   // Zurückbutton
+var stones = [], coins = [], c, s;                              // Arrays für die Elemente
+var coinPointsInterval, stonePointsInterval;                    // Intervalle für die Punkteanzeige
+var lives, life = [];                                           // Komponenten für die "Leben"
+var coinsFallen, coinsCollected;                                // Zähler für die Auswertung
+
 
 class Game_Easy extends Phaser.Scene {
 
@@ -10,7 +21,24 @@ class Game_Easy extends Phaser.Scene {
     }
 
     create(){
-        background = this.add.image(0,0,'sam');
+	/*************************************    HINTERGRUND     ***********************************************
+         Ist die Höhe des Bildes kleiner als die Höhe des Geräts, so wird die Höhe an die des Geräts angepasst
+         Ist die Breite kleiner als die des Geräts, so wird die Breite des Bildes dem des Geräts angepasst
+         Das Verhältnis des Bildes bleibt gleich, sodass sich das Bild nicht streckt
+        **/
+        background = this.add.image(0, 0, 'sam');   
+        if (background.height < game.config.height){
+            background.displayHeight = this.sys.game.config.height;
+            if(background.width < game.config.width){
+                background.displayHeight = this.sys.game.config.height;
+            }
+        }
+        else if(background.width < game.config.width){
+            background.displayWidth = this.sys.game.config.width;
+        }
+        background.scaleX = background.scaleY;
+        background.y = game.config.height/2;
+        background.x = game.config.width/2;
     }
 
 }
