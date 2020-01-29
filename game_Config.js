69e6fbd7a4c01f37e89d8312c356d7e3b5d2ccc8
Andrea <andreagrimmiger1@hotmail.com>
2020-01-29 21:27:15 +0100

    Create game_Config.js

---
+++ game_Config.js
@@ -0,0 +1,29 @@
+/**********************************************************     SCHRIFTART      ***************************************************/
+
+var new_font = new FontFace('AhkioW05-Light', 'url(Fonts/AhkioW05-Light.ttf)');
+new_font.load().then(function(loaded_face) {
+    document.fonts.add(loaded_face);
+}).catch(function(error) {
+});
+
+/**********************************************************    SETUP FÜR PHASER    ***********************************************/
+
+let config = {
+    type: Phaser.AUTO,
+    width: 900,
+    height: 1200,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    scene: [Start, Instruction, Picture_Selector, Puzzle_Game, Game_Win, Game_Over]
+};
+
+var game = new Phaser.Game(config);
+
